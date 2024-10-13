54f7f00fe52100ed5b51afec41d65816d7f01279
Nemoto99 <gaeul9717@gmail.com>
2024-10-13 23:47:32 +0900

    サーバーエラー、dockerファイル,ymlファイルを修正

--- backend/main.go
+++ backend/main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"app/controllers"
 	"app/models"
-	"log"
-	"time"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
@@ -13,7 +11,6 @@ import (
 )
 
 func main() {
-	log.Println("Starting sever...")
 
 	r := gin.Default()
 	db := ConnectDatabase()
@@ -27,31 +24,18 @@ func main() {
 	}))
 
 	r.GET("/todos", controllers.GetTodos(db))
-	r.POST("/todos", controllers.PostTodo(db))
-	r.PUT("/todos/:id", controllers.UpdateTodo(db))
-	r.DELETE("/todos/:id", controllers.DeleteTodo(db))
+	r.POST("/todo", controllers.PostTodo(db))
+	// r.PUT("/todo/:id", controllers.UpdateTodo(db))
+	r.DELETE("/todo/:id", controllers.DeleteTodo(db))
 
-	log.Println("Running server on port 8080...")
-	if err := r.Run(":8080"); err != nil {
-		log.Fatalf("Failed to run server: %v", err)
-	}
-	// r.Run(":8080")
-
+	r.Run(":8080")
 }
 
 func ConnectDatabase() *gorm.DB {
-	dsn := "docker:docker@tcp(database:3306)/main?charset=utf8mb4&parseTime=True&loc=Local"
-	var db *gorm.DB
-	var err error
-	for i := 0; i < 5; i++ {
-		db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
-		if err == nil {
-			break
-		}
-		time.Sleep(2 * time.Second)
-	}
+	dsn := "docker:docker@tcp(database)/main?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
-		panic("Failed to connect database!")
+		panic("failed to connect db")
 	}
 
 	db.AutoMigrate(&models.Todo{})
--- docker/backend.Dockerfile
+++ docker/backend.Dockerfile
@@ -1,5 +1,7 @@
 FROM golang:1.21
 
+COPY ./backend/go.mod /go/src/app/go.mod
+COPY ./backend/go.sum /go/src/app/go.sum
 
 RUN go install github.com/cosmtrek/air@v1.29.0
 
